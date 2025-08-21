0a521a3f2a9a094f26fdafea77c7884cb3ec87f5
Rahul-jfs <ra1290hul@gmail.com>
2025-08-21 12:48:23 +0530

    protected Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import { AuthProvider } from "./utils/AuthProvider";
 import { HabitProvider } from "./utils/HabitProvider";
 import Dashboard from "./components/Dashboard";
 import ThemeProvider from "./utils/ThemeProvider";
+import { useAuth } from "./utils/AuthContext";
+
+const Protected = ({ children }) => {
+  const { currentUser } = useAuth();
+  return currentUser ? children : <Navigate to="/" />;
+};
 
 const App = () => {
   return (
@@ -13,7 +19,14 @@ const App = () => {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<LoginPage />} />
-              <Route path="/dashboard" element={<Dashboard />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <Protected>
+                    <Dashboard />
+                  </Protected>
+                }
+              />
             </Routes>
           </BrowserRouter>
         </HabitProvider>
